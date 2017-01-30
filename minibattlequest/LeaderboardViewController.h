45ff8abfee1540b8ca970e02e7a953b425660c1a
Erick <efernandezcraft@gmail.com>
2017-01-30 13:53:35 -0800

    Added main menu and leaderboard scenes

---
+++ minibattlequest/LeaderboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController_MainMenuViewController.h
+//  minibattlequest
+//
+//  Created by Erick Fernandez de Arteaga on 2017-01-30.
+//  Copyright © 2017 Mini Battle Quest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeaderboardViewController : UIViewController
+
+@end
