bc1c327c7b4e690f37c1ee17bb9f743f2b73b2da
Chris Leclair <xcvgvcx@gmail.com>
2017-01-31 12:40:58 -0800

    added some classes

---
+++ minibattlequest/EnemyObject.m
@@ -0,0 +1,38 @@
+//
+//  EnemyObject.m
+//  minibattlequest
+//
+//  Created by Chris on 2017-01-31.
+//  Copyright © 2017 Mini Battle Quest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EnemyObject.h"
+
+@interface EnemyObject()
+{
+    
+    
+}
+@end
+
+@implementation EnemyObject
+
+//we should override these (are they virtual by default like Java or not like C++?)
+-(id)init
+{
+    self = [super init];
+    return self;
+}
+
+-(void)update
+{
+    
+}
+
+-(void)display
+{
+    
+}
+
+@end
---
+++ minibattlequest/PlayerObject.h
@@ -0,0 +1,18 @@
+//
+//  PlayerObject.h
+//  minibattlequest
+//
+//  Created by Chris on 2017-01-31.
+//  Copyright © 2017 Mini Battle Quest. All rights reserved.
+//
+
+#ifndef PlayerObject_h
+#define PlayerObject_h
+
+#import "GameObject.h"
+
+@interface PlayerObject : GameObject
+
+@end
+
+#endif /* PlayerObject_h */
